a0a183f4cbae87c01b69b818a9026f392338536b
Jonathan Gilyadov <jonathan.gilyadov@gmail.com>
2025-04-18 17:40:43 +0300

    serving build

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/api");
 const errorHandler = require("./middleware/errorHandler");
 const anonIdMiddleware = require("./middleware/anonIdMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(anonIdMiddleware);
@@ -11,6 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(errorHandler);
 app.use("/api", apiRoutes);
+
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
